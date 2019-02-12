8247b53b52a9ecfd69fa75cf01470f6e5e93cf16
Andrés González <a.gonzalez.terres@gmail.com>
2019-02-11 22:25:00 -0300

    primera version, la comiteo para no mandarme cagada. ahora lo pongo en otro proyecto

--- palan-card/src/main/scala/palan/call-centre/package.scala
+++ palan-card/src/main/scala/palan/call-centre/package.scala
@@ -12,31 +12,35 @@ import scala.concurrent.{ExecutionContext, Future}
 
 package object CallCentre {
 
-  implicit val ec: ExecutionContext = ExecutionContext global
+  implicit val executionContext: ExecutionContext = ExecutionContext global
   implicit val actorSystem: ActorSystem = ActorSystem()
 
   type AgentId = Id
 
   sealed abstract class Call {
-    def clientId: ClientId
+    def client: Client
+  }
+
+  case class Client(id: ClientId) extends Identifiable {
+    def speak: Future[Unit] = after(7 seconds, actorSystem scheduler, executionContext, Future())
   }
 
   trait Triaged {
     def agent: Agent
   }
 
-  case class IncomingCall(clientId: ClientId, incomingTime: LocalDateTime = LocalDateTime now) extends Call {
-    def open(agent: Agent): OpenedCall = OpenedCall(this, agent)
+  case class IncomingCall(client: Client, incomingTime: LocalDateTime = LocalDateTime now) extends Call {
+    def open(agent: Agent, dateTime: => LocalDateTime): OpenedCall = OpenedCall(this, agent, dateTime)
   }
 
-  case class OpenedCall(incomingCall: IncomingCall, agent: Agent, openTime: LocalDateTime = LocalDateTime now) extends Call with Triaged {
-    override def clientId: ClientId = incomingCall clientId
+  case class OpenedCall(incomingCall: IncomingCall, agent: Agent, openTime: LocalDateTime) extends Call with Triaged {
+    override def client: Client = incomingCall client
 
-    def close: ClosedCall = ClosedCall(this)
+    def close(dateTime: => LocalDateTime): ClosedCall = ClosedCall(this, dateTime)
   }
 
-  case class ClosedCall(openedCall: OpenedCall, closeTime: LocalDateTime = LocalDateTime now) extends Call with Triaged {
-    override def clientId: ClientId = openedCall clientId
+  case class ClosedCall(openedCall: OpenedCall, closeTime: LocalDateTime) extends Call with Triaged {
+    override def client: Client = openedCall client
 
     override def agent: Agent = openedCall agent
   }
@@ -44,44 +48,64 @@ package object CallCentre {
   case class Agent(id: AgentId) extends Identifiable {
 
     def answer(incomingCall: IncomingCall): Future[ClosedCall] = {
-      val openedCall = incomingCall open this
-      after(5 minutes, actorSystem scheduler, ec,
-        Future(openedCall close)
-      )
+
+      val openedCall = incomingCall.open(this, LocalDateTime now)
+
+      for {
+        _ <- openedCall.client.speak
+      } yield openedCall.close(LocalDateTime.now)
+
     }
 
   }
 
-  case class State(busy: Set[Agent], free: Queue[Agent]) {
+  case class State(free: Queue[Agent], busy: Set[Agent] = Set empty) {
 
     val MAX_BUSY_AGENTS = 10
-
-    def dispatch(call: Call): Call => State = Dispatcher dispatch this
 
     def assign(incomingCall: IncomingCall): State =
       if (busy.size == MAX_BUSY_AGENTS) throw new Exception("all busy")
       else {
         val (agent, newFree) = free dequeue
 
-        agent answer incomingCall map dispatch
+        agent answer incomingCall map Dispatcher.dispatch
 
-        this copy(busy + agent, newFree)
+        this copy(newFree, busy + agent)
       }
 
     def unAssign(closedCall: ClosedCall): State = {
       val agent = closedCall agent
 
-      this copy(busy - agent, free enqueue agent)
+      this copy(free enqueue agent, busy - agent)
     }
 
   }
 
   object Dispatcher {
 
-    def dispatch(state: State)(call: Call): State = call match {
-      case c: IncomingCall => state assign c
-      case c: ClosedCall => state unAssign c
-      case _ => state
+    var state = State((1 to 30 map Agent).foldLeft(Queue[Agent]())(_ enqueue _))
+
+    def dispatch(call: Call): Unit = {
+
+      println()
+      println("////////////////////////////////////////////////////////")
+      println()
+      println(s"prevState: $state")
+      println()
+      println(s"call: $call")
+      println()
+
+      call match {
+        case c: IncomingCall => state = state assign c
+        case c: ClosedCall => state = state unAssign c
+        case _ =>
+      }
+
+      println(s"newState: $state")
+      println()
+      println("////////////////////////////////////////////////////////")
+
+
     }
 
   }
