05ccf95619366bb02ce4465d49910fafabacfc8c
Andrés González <a.gonzalez.terres@gmail.com>
2019-02-11 23:21:26 -0300

    solved call

--- palan-card/src/main/scala/palan/call-centre/package.scala
+++ palan-card/src/main/scala/palan/call-centre/package.scala
@@ -17,45 +17,61 @@ package object CallCentre {
 
   type AgentId = Id
 
+  trait Client extends Identifiable
+
+  case class PendingClient(id: ClientId) extends Client {
+    def activate(openedCall: OpenedCall): ActiveClient = ActiveClient(id, openedCall)
+  }
+
+  case class ActiveClient(id: ClientId, openedCall: OpenedCall) extends Client {
+    def speak: Future[SolvedCall] = after(7 seconds, actorSystem scheduler, executionContext, Future(openedCall solve))
+  }
+
   sealed abstract class Call {
     def client: Client
   }
 
-  case class Client(id: ClientId) extends Identifiable {
-    def speak: Future[Unit] = after(7 seconds, actorSystem scheduler, executionContext, Future())
-  }
-
   trait Triaged {
     def agent: Agent
+    def client: ActiveClient
   }
 
-  case class IncomingCall(client: Client, incomingTime: LocalDateTime = LocalDateTime now) extends Call {
+  case class IncomingCall(client: PendingClient, incomingTime: LocalDateTime = LocalDateTime now) extends Call {
     def open(agent: Agent, dateTime: => LocalDateTime): OpenedCall = OpenedCall(this, agent, dateTime)
   }
 
-  case class OpenedCall(incomingCall: IncomingCall, agent: Agent, openTime: LocalDateTime) extends Call with Triaged {
-    override def client: Client = incomingCall client
+  case class OpenedCall(incomingCall: IncomingCall, agent: Agent, openedTime: LocalDateTime) extends Call with Triaged {
+
+    override def client: ActiveClient = incomingCall.client activate this
 
     def close(dateTime: => LocalDateTime): ClosedCall = ClosedCall(this, dateTime)
+
+    def solve: SolvedCall = SolvedCall(this)
+
   }
 
-  case class ClosedCall(openedCall: OpenedCall, closeTime: LocalDateTime) extends Call with Triaged {
-    override def client: Client = openedCall client
+  case class SolvedCall(openedCall: OpenedCall) extends Call with Triaged {
+
+    override def client: ActiveClient = openedCall client
 
     override def agent: Agent = openedCall agent
+
+    def close(dateTime: => LocalDateTime): ClosedCall = ClosedCall(this, dateTime)
+
+  }
+
+  case class ClosedCall(openedCall: Call with Triaged, closedTime: LocalDateTime) extends Call with Triaged {
+
+    override def client: ActiveClient = openedCall client
+
+    override def agent: Agent = openedCall agent
+
   }
 
   case class Agent(id: AgentId) extends Identifiable {
 
-    def answer(incomingCall: IncomingCall): Future[ClosedCall] = {
-
-      val openedCall = incomingCall.open(this, LocalDateTime now)
-
-      for {
-        _ <- openedCall.client.speak
-      } yield openedCall.close(LocalDateTime.now)
-
-    }
+    def answer(incomingCall: IncomingCall): Future[ClosedCall] =
+      incomingCall.open(this, LocalDateTime now).client.speak.map(_ close (LocalDateTime now))
 
   }
 
